9014ade0b6ce40191e183f306d2fb1e1359d29eb
Andrey <apav1987@gmail.com>
2022-08-06 19:15:02 +0300

    example1

--- example001/Program.cs
+++ example001/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("2Hello, World!");
+﻿
+Console.WriteLine("введите два числа");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+if(n > m)
+{
+    Console.WriteLine("наибольшее число"+n);
+}
+else
+{
+    Console.WriteLine("наибольшее число"+m);
+}
+
